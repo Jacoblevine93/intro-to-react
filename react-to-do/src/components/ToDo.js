c10618194a87ec177c8f672f2b7214901fe27acf
Jacob Levine <Jacob.levine93@protonmail.com>
2018-09-01 15:28:58 -0700

    Checkpoint 8 Events: completed

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -5,7 +5,7 @@ class ToDo extends Component {
 	render() {
 		return(
 			<li>
-				<input type="checkbox" checked={ this.propsisCompleted } />
+				<input type="checkbox" checked={ this.propsisCompleted } onChange={this.props.toggleComplete} />
 				<span> {this.props.description} </span>
 
 			</li>
